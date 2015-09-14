1f2198bd5f53cc0ee06632696434fa9246ca864b
Tim Chase <tim@onlight.com>
2015-09-14 16:25:22 -0500

    Add per-mount uid/gid override
    The new inheritid filesystem property can be set to "uid", "gid" or "all".
    It is interpreted by the zfs.mount helper to set the temporary mount options
    indicating the uid and/or gid of all files and directories in the filesystem
    should be overridden to match that of, respectively, the uid or gid of the
    directory on which the filesystem is being mounted.
    
    This is useful for pools on removable media which are transported between
    systems without synchronized user and group ids.
    
    Additionally, the zfs.mount helper will interpret any uid=<id> or gid=<id>
    mount options it is passed on the command line the ownership or group
    override to be manually specified to something other than those of the
    directory on which the filesystem is being mounted.
    
    A uid override causes all existing files to appear to be owned by the
    specified user id.  It also causes newly-created files to inherit the
    uid of their containing directory.  Group ids are handled similarly.
    
    Since posix ACLs are specified as integer uid/gid values, they are
    incompatible with this feature.  Using either uid or gid override has
    the effect of setting acltype=noacl.

--- include/sys/fs/zfs.h
+++ include/sys/fs/zfs.h
@@ -184,6 +184,7 @@ typedef enum {
 	ZFS_PROP_REMAPTXG,		/* not exposed to the user */
 	ZFS_PROP_SPECIAL_SMALL_BLOCKS,
 	ZFS_PROP_IVSET_GUID,		/* not exposed to the user */
+	ZFS_PROP_INHERITID,
 	ZFS_NUM_PROPS
 } zfs_prop_t;
 
@@ -454,6 +455,13 @@ typedef enum zfs_key_location {
 
 #define	DEFAULT_PBKDF2_ITERATIONS 350000
 #define	MIN_PBKDF2_ITERATIONS 100000
+
+typedef enum {
+	ZFS_INHERITID_OFF,
+	ZFS_INHERITID_UID,
+	ZFS_INHERITID_GID,
+	ZFS_INHERITID_ALL
+} zfs_inheritid_type_t;
 
 /*
  * On-disk version number.
